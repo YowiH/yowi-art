17806877c1e2078b4c60d1d9afa9551e37f8c95f
YowiH <youssef.houdaibi06@gmail.com>
2023-04-18 00:02:33 +0200

    v1.0.0 - Base

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const categories = document.querySelectorAll(".category");
-const activeImage = null;
+let activeImage = null;
 categories.forEach(image => {
     image.addEventListener("click", () => {
         const categoryImgID = image.dataset.categoryimg;
         const categoryImg = document.getElementById(categoryImgID);
         categoryImg.classList.toggle("active");
+        image.classList.toggle("active");
     });
 });
